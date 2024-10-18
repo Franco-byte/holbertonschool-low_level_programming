9a1e0677be2d58fcc0320b9b3b08fabbac9fa1f2
Franco-byte <fd.reyes3110@gmail.com>
2024-10-18 19:27:00 +0000

    1_5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Utilizando printf
+ *
+ * Return: No tiene que dar nada en especial
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}	
