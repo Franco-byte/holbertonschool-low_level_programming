0f91710ada8ac04d1467aacd1c3d0acf091d7054
Franco-byte <fd.reyes3110@gmail.com>
2024-10-22 18:27:49 +0000

    2_6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - ta rara esta
+ * Return: 0
+ */
+
+int main(void)
+{
+	int nrs;
+
+	for (nrs = '0'; nrs <= '9'; nrs++)
+		putchar(nrs);
+	putchar('\n');
+
+	return (0);
+}
