5b7f97220153f4e110d5cf2b412315fa8a7e469f
Franco-byte <fd.reyes3110@gmail.com>
2024-10-24 19:24:45 +0000

    3_3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Identificar misnusculas
+ * @c: Variable desconocida
+ *
+ * Return: cerooo
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+
+}
