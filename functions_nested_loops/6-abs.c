ee206bfcdab8d49c7be21b553e0c7b1a4df68a23
Franco-byte <fd.reyes3110@gmail.com>
2024-10-24 22:10:10 +0000

    3_6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Numeros absolutos
+ *@num: variable para el numero absoluto
+ *
+ * Return: el numero absoluto en positivo
+ */
+
+int _abs(int num)
+{	
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
+}
