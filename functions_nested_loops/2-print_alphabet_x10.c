bdb3485f70ad9bf2cc88866e5a5b434826aea725
Franco-byte <fd.reyes3110@gmail.com>
2024-10-24 18:14:44 +0000

    3_2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alfabeto 10 veces
+ * Return: cero
+ */
+
+void print_alphabet_x10(void)
+{
+	int abc;
+
+	for (abc = '0'; abc <= '9'; abc++)
+	{
+		print_alphabet();
+	}
+}
