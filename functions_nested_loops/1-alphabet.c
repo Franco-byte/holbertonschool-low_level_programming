03d19f1a7a1ba73349f64461b6c7497946862f3b
Franco-byte <fd.reyes3110@gmail.com>
2024-10-24 16:48:13 +0000

    3_1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprimir alfabeto
+ * Return: cero
+ */
+
+void print_alphabet(void)
+{
+	int abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar(abc);
+	}
+		_putchar('\n');
+}
