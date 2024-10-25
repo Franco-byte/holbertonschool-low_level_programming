00aa4bb78b42c23389781c39584d13f50835d047
Franco-byte <fd.reyes3110@gmail.com>
2024-10-25 19:44:46 +0000

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void times_table(void)
+{
+	int m, mx, result;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (mx = 0; mx <= 9; mx++)
+		{
+		result = m * mx;
+			if (result < 10)
+			{
+				_putchar(result +'0');
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			
+				if (mx < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
+		}
+	_putchar('\n');
+	}
+}
