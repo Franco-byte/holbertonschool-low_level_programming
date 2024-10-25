6889a6de634c09fd7c65beea7dc5a1e3c6ee9c29
Franco-byte <fd.reyes3110@gmail.com>
2024-10-25 21:26:38 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - suma
+ * @num1: numero uno
+ * @num2: numero dos
+ *
+ * Return: la suma de las variables proporcionadas
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
