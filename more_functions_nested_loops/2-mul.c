1d18340c6d700b91954f2c3d09aa07eec7861c0d
Franco-byte <fd.reyes3110@gmail.com>
2024-10-28 16:54:16 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplica dos variables
+ * @num1: multipo
+ * @num2: Multiplicador
+ *
+ * Return: Resultado de la multiplicacion
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
