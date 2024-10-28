cba9224434a7647b2a2ac49abd7993107e132f36
Franco-byte <fd.reyes3110gmail.com>
2024-10-28 00:09:16 +0000

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Va hacia el 98
+ * @n: Numero de inicio 
+ *
+ * Return: nada
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n !=98)
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			printf(", ");
+		}
+	}
+}
