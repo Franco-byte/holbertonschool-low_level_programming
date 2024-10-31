9d0452c47c0f5e3ebf369796992fb2407fd7415c
Franco-byte <fd.reyes3110@gmail.com>
2024-10-31 17:53:33 +0000

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
+ * reset_to_98 - Actualiza el valor de la variable a la que apunta n a 98
+ * @n: puntero a un entero que será actualizado
  *
+ * Return: nada
  */
 
 void reset_to_98(int *n)
