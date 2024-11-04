a6e472be5cb0bdc1b92fdeee4c50ec8285707af4
Franco-byte <fd.reyes3110@gmail.com>
2024-11-04 16:09:11 +0000

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
 /**
+ * *_memset - Espacios de memoria
+ * @s: Espacio de memoria
+ * @b: Variable con el contenido
+ * @n: numero de veces que se repite b en s
  *
+ * Return: El contenido de s
  */
 
 char *_memset(char *s, char b, unsigned int n)
