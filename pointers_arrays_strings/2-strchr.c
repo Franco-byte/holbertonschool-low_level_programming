a9b942eaa0c9a2274c8de73b8e02a70493958600
Franco-byte <fd.reyes3110@gmail.com>
2024-11-05 15:23:26 +0000

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,4 +6,12 @@
 
 char *_strchr(char *s, char c)
 {
+	int iterador;
 
+	for (iterador = 0; s[iterador] != '\0'; iterador++)
+	{
+		if (s[iterador] == c)
+			return &s[iterador];
+	}
+	return (NULL);
+}
