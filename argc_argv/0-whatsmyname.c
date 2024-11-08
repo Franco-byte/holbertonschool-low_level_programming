4fd7d364c21a72241ab791e8ee6a6030284d3ad2
Franco-byte <fd.reyes3110@gmail.com>
2024-11-08 23:43:09 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Imprime la poscion de argv
+ * @argc: Dal la posicion de los elementos dentro de argv
+ * @argv: Da la posicion de tod el string
+ *
+ * Return: cero
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stddef.h>
+
+int _putchar(char c);
