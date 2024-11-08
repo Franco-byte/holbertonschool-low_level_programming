e9aabd6171f65aba4d8e929c6d15d9485e021edb
Franco-byte <fd.reyco3110@gmail.com>
2024-11-08 16:34:59 +0000

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
 /**
+ * _pow_recursion - Dal el factorial de un numero dado
+ * @x: Numero 1
+ * @y: Numero 2
  *
- *
+ * Return: El factorial de x * y o -1
  */
 
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
