07b09db54b9b6738addd501db1b261d4d9134f56
Franco-byte <fd.reyes3110@gmail.com>
2024-11-09 00:36:35 +0000

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - imprime argumentos
+ * @argc: cuenta rgumentos
+ * @argv: almacena los argumentos
+ * 
+ * Return: cero
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
