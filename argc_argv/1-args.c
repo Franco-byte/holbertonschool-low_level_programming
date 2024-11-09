20a304a1d5c52cc8b5ef44b77db395b6ed50631a
Franco-byte <fd.reyes3110@gmail.com>
2024-11-09 00:13:20 +0000

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: cero
  */
 
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	argc = argc -1;
+	argc = argc - 1;
 
 	printf("%d\n", argc);
 	return (0);
