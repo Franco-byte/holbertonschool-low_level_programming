06281eaa8eb3f17ee06a3f2b90fe1031edd7bd3a
Franco-byte <fd.reyes3110@gmail.com>
2024-11-10 17:26:29 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - imprimir string en reversa
+ * @s: la string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int var;
+
+	for (var = 0; s[var] != '\0'; var++)
+	{
+	}
+
+	for (var = var - 1; s[var] >= 0; var--)
+	{
+		_putchar(s[var]);
+	}
+	_putchar('\n');
+}
