e1f26e0ae6de76f8a81fb8535e58b76b745ae222
Franco-byte <fd.reyes3110@gmail.com>
2024-11-14 23:45:54 +0000

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 /**
  * *_calloc - Asigna espacios de memoria para un array
  * @nmemb: cantidad de elementos
- * @size: ramaño en bytes
+ * @size: ramaño en byte
  *
  * Return: p
  */
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	p = malloc(nmemb * size);
 
 	if (p == NULL)
