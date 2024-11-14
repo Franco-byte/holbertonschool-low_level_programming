af201458fbda9695a6bf0e3956a3e8643adbdc7e
Franco-byte <fd.reyes3110@gmail.com>
2024-11-14 23:22:13 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * Return: num
  */
 
-int _strlen(char *p)
+unsigned int _strlen(char *p)
 {
 	unsigned int num = 0, i;
 
@@ -19,27 +19,34 @@ int _strlen(char *p)
 }
 
 /**
+ * *string_nconcat - Concatena 2 strings
+ * @s1: string uno
+ * @s2: string dos
+ * @n: maximo tama√o de caracteres para el string dos
  *
- *
- *
+ * Return: arr con los strings unidos
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *arr;
-	unsigned int i, i2, i3, limit;
+	unsigned int i, i2, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n < _strlen(s2)
+	if (n < _strlen(s2))
+	{
 		limit = n;
 		arr = malloc(_strlen(s1) + n + 1);
+	}
 	else
+	{
 		limit = _strlen(s2);
 		arr = malloc(_strlen(s1) + _strlen(s2) + 1);
+	}
 
 	if (arr == NULL)
 		return (NULL);
@@ -47,16 +54,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < _strlen(s1); i++)
 		arr[i] = s1[i];
 	
-	if (n < _strlen(s2))
-	{
-		for (i3 = 0; i3 < n; i3++)
-			arr[i + i3] = s2[i3];
-	}
-	else
-	{
-		for (i2 = 0; i2 < _strlen(s2); i2++)
-			arr[i + i2] = s2[i2];
-	}
+	for (i2 = 0; i2 < limit; i2++)
+		arr[i + i2] = s2[i2];
+	
 	arr[i + limit] = '\0';
 	return (arr);
 }
