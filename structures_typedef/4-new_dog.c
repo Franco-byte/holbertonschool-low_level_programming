de3542c9fac41b5b912e79a1a9e6c7dd807cdc30
Franco-byte <fd.reyes3110@gmail.com>
2024-11-17 19:36:56 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include "dog.h"
 
 /**
+ * *_strcpy - Copia contenido de un array a otro
+ * @cpy: Destino de la copia
+ * @orig: Tiene el contenido a copiar
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *cpy, char *orig)
+{
+	int i;
+
+	for (i = 0; orig[i] != '\0'; i++)
+		cpy[i] = orig[i];
+	cpy[i] = '\0';
+
+	return (cpy);
+}
+
+/**
+ * _strlen - cuenta caracteres
+ * @p: string a contar
+ *
+ * Return: num
+ */
+
+int _strlen(char *p)
+{
+	int num = 0, i;
+
+	for (i = 0; p[i] != '\0'; i++)
+		num++;
+	return (num);
+}
+
+/**
  * new_dog - Creates a new dog
  * @name: Name of the dog
  * @age: Age of the dog
@@ -19,16 +54,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(_strlen(name) + 1);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	strcpy(name_copy, name);
+	_strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(_strlen(owner) + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
@@ -36,7 +71,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	strcpy(owner_copy, owner);
+	_strcpy(owner_copy, owner);
 
 	new_dog->name = name_copy;
 	new_dog->age = age;
