c910c91cd27b90fa373ec9744f3386dbe4defb0e
Franco-byte <fd.reyes3110@gmail.com>
2024-12-05 22:51:45 +0000

    2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,17 +1,36 @@
 #include "lists.h"
 
 /**
+ * add_node - Add a new node to an existing list
+ * @head: Pointer to pointer to 'head' list
+ * @str: Element to copy for the new node
  *
- *
- *
+ * Return: The address to the new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_nodes = malloc(sizeof(list_t));
+	int i, count = 0;
 
-	if (new_nodes == NULL)
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
-	list_t *new_nodes = strdup(str);
+	new_node->str = strdup(str);
 
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new_node->len = count;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
