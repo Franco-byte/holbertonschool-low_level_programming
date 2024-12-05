b2a4f45a25c086c7d683b5d88846efc1f361d874
Franco-byte <fd.reyes3110@gmail.com>
2024-12-05 16:49:05 +0000

    0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Print the nodes of linked lists
+ * @h: Pints to the nodes to use in the funcion
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			count++;
+		}
+	}
+	return (count);
+}
